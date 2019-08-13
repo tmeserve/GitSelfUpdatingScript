eae54b51d2b46f90bd110828a68e7fd533842900
Tyler <tyler.m.meserve@gmail.com>
2019-08-13 20:05:46 +0100

    Changed the status check

--- test_update.py
+++ test_update.py
@@ -15,7 +15,7 @@ def CheckForUpdate(workingDir):
     time.sleep(2)
     print("Checking status for " + workingDir + "...")
     statusCheck = git('status')
-
+    print(statusCheck, ' is the status')
     if "Your branch is up-to-date" in statusCheck:
         print("Status check passes.")
         print("Code up to date.")
